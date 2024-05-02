1f84c04daf8fc4d13d32bda115633014c56504bd
Jaredgg4 <jg70456898@gmail.com>
2024-05-02 13:26:25 -0600

    Added an a feature that inputs a number and uses that value to increment and decrement by that amount and a history of the inputs with a reset history button.

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -4,21 +4,46 @@ import styles from './counter.module.css';
 
 const Counter: React.FC = () => {
     const [number, setNumber] = useState(0);
+    const [inc, setInc] = useState<string>('');
+    const [list, setList] = useState<string[]>([]);
+
+
     
     function addNum(){
-        setNumber(number + 1);
+        setList([...list, inc]);
+        setNumber(number + parseFloat(inc));
     }
 
     function subNum(){
-        setNumber(number - 1);
+        setList([...list, inc]);
+        setNumber(number - parseFloat(inc));
     }
+
+
+
+
+   function reset(){
+    setList([]);
+   }
+
 
 
     return (
         <div className={styles.counter}>
+            Add/Sub by: <form>
+            <input type="text" value={inc} onChange={(e: any) => setInc(e.target.value)}></input>
+            </form>
             <p>counter: {number}</p>
             <button onClick={addNum}>Increment</button>
             <button onClick={subNum}>Decrement</button>
+            <button onClick={reset}>Reset History</button>
+            <h3>History: </h3>
+            <ul>
+            {list.map((value: any, index: any)=> (
+                    <li key={index}>{value}</li>
+                ))}
+            </ul>
+        
         </div>
     )
 }
